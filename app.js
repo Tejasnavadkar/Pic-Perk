9e772bb4681a22136bba60ea442de57fee549a67
Tejasnavadkar <tejas.navadkar@gmail.com>
2024-05-07 20:25:59 +0530

    add cors in app

--- app.js
+++ app.js
@@ -14,6 +14,15 @@ const passport = require("passport");
 
 var app = express();
 
+const cors = require('cors')
+const corsConfig = {
+    origin:"*",
+    credential: true,
+    methods:["GET","POST","PUT","DELETE"],
+};
+app.options("",cors(corsConfig));
+app.use(cors(corsConfig));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
